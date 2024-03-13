d33395495ac3e77433ade9925b8c798b7069c74c
anybody723 <anybody723@proton.me>
2024-03-13 08:54:59 -0300

    figurinhas

---
+++ Maratona/cmake-build-debug/CMakeFiles/Maratona.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/anybody/Documents/Projects/Projects_SI1/Maratona/C++/Lista1/amigo.cpp" "CMakeFiles/Maratona.dir/C++/Lista1/amigo.cpp.o" "gcc" "CMakeFiles/Maratona.dir/C++/Lista1/amigo.cpp.o.d"
+  "/home/anybody/Documents/Projects/Projects_SI1/Maratona/C++/Lista1/figurinhas.cpp" "CMakeFiles/Maratona.dir/C++/Lista1/figurinhas.cpp.o" "gcc" "CMakeFiles/Maratona.dir/C++/Lista1/figurinhas.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Maratona/cmake-build-debug/CMakeFiles/Maratona.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Maratona.
+# This may be replaced when dependencies are built.
