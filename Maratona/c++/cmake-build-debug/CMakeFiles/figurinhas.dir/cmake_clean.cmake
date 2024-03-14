3c4be5e65e967f84c07a56463bb4365794952102
anybody723 <anybody723@proton.me>
2024-03-14 20:39:49 -0300

    third exercise not working

---
+++ Maratona/c++/cmake-build-debug/CMakeFiles/figurinhas.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/figurinhas.dir/Lista1/figurinhas.cpp.obj"
+  "CMakeFiles/figurinhas.dir/Lista1/figurinhas.cpp.obj.d"
+  "figurinhas.exe"
+  "figurinhas.exe.manifest"
+  "figurinhas.pdb"
+  "libfigurinhas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/figurinhas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
