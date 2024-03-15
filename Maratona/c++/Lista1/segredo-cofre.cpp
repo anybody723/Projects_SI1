3addbc85bc9fd6f8aa742873daa12de1d19ad6ac
anybody723 <anybody723@proton.me>
2024-03-15 14:17:23 -0300

    third exercise done

--- Maratona/c++/Lista1/segredo-cofre.cpp
+++ Maratona/c++/Lista1/segredo-cofre.cpp
@@ -3,71 +3,36 @@
 using namespace std;
 
 int main() {
-
     int N, M;
     cin >> N >> M;
-
-    cin.ignore();
-
     int posicoes[N];
     for (int i = 0; i < N; i++) {
         cin >> posicoes[i];
     }
-
     int deslizes[M];
     for (int i = 0; i < M; i++) {
         cin >> deslizes[i];
         deslizes[i] = deslizes[i] - 1;
     }
-
     int quantVezes[10];
-    /*for (int i = 0; i < 10; i++) {
-        quantVezes[i] = 0;
-    }*///suggestion
     for (int &quantVez: quantVezes) {
         quantVez = 0;
     }
-
-    for(int i = 1; i < M; i++){
+    int j;
+    for(int i = 0; i < M; i++){
         int anterior;
         if(i == 0){
-            anterior = 1;
+            anterior = 0;
+            i++;
         }
-        for (int j = anterior; j != deslizes[i]; (deslizes[i] < anterior) ? j-- : j++) {
+        for (j = anterior; j != deslizes[i]; (deslizes[i] < anterior) ? j-- : j++) {
             quantVezes[posicoes[j]]++;
         }
         anterior = deslizes[i];
     }
-
-    /*for (int i = 0; i < M; i++) {
-        cout << "For: " << i << endl;
-        if (i == 0) {
-            for (int j = deslizes[i] - 1; j != deslizes[i + 1] - 1 + 1; j++) {
-                quantVezes[posicoes[j]]++;
-                cout << "For1: " << posicoes[j] << endl;
-                for (int i = 0; i < 10; ++i) {
-                    cout << "quantVezes[" << i << "] = " << quantVezes[i] << endl;
-                }
-            }
-        } else if (i != 3 && deslizes[i - 1] < deslizes[i + 1] + 1) {
-            for (int j = deslizes[i]; j != deslizes[i + 1] - 1; j++) {
-                quantVezes[posicoes[j]]++;
-                cout << "For2: " << posicoes[j] << endl;
-            }
-        } else {
-            for (int j = deslizes[i - 1]; j != deslizes[i - 1] - 1; j--) {
-                quantVezes[posicoes[j]]++;
-                cout << "For3: " << posicoes[j] << endl;
-            }
-        }
-    }*/
-
+    quantVezes[posicoes[j]]++;
     for (int i = 0; i < 10; ++i) {
-        cout << "quantVezes[" << i << "] = " << quantVezes[i] << endl;
+        cout << quantVezes[i] << " ";
     }
-    /*5 4
-5 8 0 5 1
-1 4 2 5
-*/
     return 0;
 }
