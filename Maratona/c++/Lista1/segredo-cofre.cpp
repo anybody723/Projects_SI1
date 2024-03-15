f5ca3244e1bc9a3268bdefd914ae15302effbf20
anybody723 <anybody723@proton.me>
2024-03-15 13:49:27 -0300

    progress in the third exercise

--- Maratona/c++/Lista1/segredo-cofre.cpp
+++ Maratona/c++/Lista1/segredo-cofre.cpp
@@ -17,6 +17,7 @@ int main() {
     int deslizes[M];
     for (int i = 0; i < M; i++) {
         cin >> deslizes[i];
+        deslizes[i] = deslizes[i] - 1;
     }
 
     int quantVezes[10];
@@ -25,6 +26,17 @@ int main() {
     }*///suggestion
     for (int &quantVez: quantVezes) {
         quantVez = 0;
+    }
+
+    for(int i = 1; i < M; i++){
+        int anterior;
+        if(i == 0){
+            anterior = 1;
+        }
+        for (int j = anterior; j != deslizes[i]; (deslizes[i] < anterior) ? j-- : j++) {
+            quantVezes[posicoes[j]]++;
+        }
+        anterior = deslizes[i];
     }
 
     /*for (int i = 0; i < M; i++) {
@@ -49,34 +61,6 @@ int main() {
             }
         }
     }*/
-
-    for (int i = 0; i < M; i++) {
-        cout << "For: " << i << endl;
-        if (i == 1) {
-            for (int j = deslizes[i] - 1; j != deslizes[i + 1] - 1 + 1; j++) {
-                quantVezes[posicoes[j]]++;
-                cout << "For1: " << posicoes[j] << endl;
-                for (int i = 0; i < 10; ++i) {
-                    cout << "quantVezes[" << i << "] = " << quantVezes[i] << endl;
-                }
-            }
-        } else if (i == 2) {
-            for (int j = deslizes[i]; j != deslizes[i + 1] - 1; j++) {
-                quantVezes[posicoes[j]]++;
-                cout << "For2: " << posicoes[j] << endl;
-            }
-        } else if (i == 3) {
-            for (int j = deslizes[i - 1]; j != deslizes[i - 1] - 1; j--) {
-                quantVezes[posicoes[j]]++;
-                cout << "For3: " << posicoes[j] << endl;
-            }
-        } else {
-            for (int j = deslizes[i - 1]; j != deslizes[i - 1] - 1; j--) {
-                quantVezes[posicoes[j]]++;
-                cout << "For3: " << posicoes[j] << endl;
-            }
-        }
-    }
 
     for (int i = 0; i < 10; ++i) {
         cout << "quantVezes[" << i << "] = " << quantVezes[i] << endl;
